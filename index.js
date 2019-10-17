47db850a299dec9b000b6b834502713f20480d23
jiangxiaoxin <jiangyanxin1989@gmail.com>
2019-10-17 17:40:19 +0800

    node版本，文件修改后重新编译，可使用es6语法，可import/export

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// require('babel-polyfill')
-// require('babel-register')
-
 console.log(1)
 console.log(2)
 
-import { add } from './src/utils/index'
+import { add, multi } from './src/utils/index'
 
 console.log(add(1, 2))
 
 console.log(4)
 
+console.log(multi(3, 4))
+
 const date = require('./src/utils/date')
 
 date.format('hello')
+
+date.format('world')
